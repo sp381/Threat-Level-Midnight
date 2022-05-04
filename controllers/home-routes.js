fe26fb61cf4104aed6f0f924d9e095668c75e2cb
Carl Bush <CarlBushs13@gmail.com>
2022-05-04 14:11:54 -0700

    fix background, placeholder get request homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,13 +2,27 @@ const router = require("express").Router();
 //const { User } = require("../models");
 
 //HOME PAGE RENDER NAME = HANDLEBARS FILE NAME
-//http://localhost:3001/
+http://localhost:3001/
 router.get("/", (req, res) => {
-    res.render("login");
+    res.render("movies");
 });
+
+
+
 //http://localhost:3001/movies = "movies" FILE NAME
 router.get("/movies", (req, res) => {
     res.render("movies");
 });
+
+//http://localhost:3001/sign-up = "movies" FILE NAME
+router.get("/sign-up", (req, res) => {
+    res.render("sign-up");
+});
+
+//http://localhost:3001/login = "movies" FILE NAME
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
 
 module.exports = router;
