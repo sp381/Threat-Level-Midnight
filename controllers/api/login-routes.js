10483b7e36de1573e650cf36bc893ec34ba68bff
Carl Bush <CarlBushs13@gmail.com>
2022-05-04 08:09:13 -0700

    homepage/login and movies URL generate HTML

--- controllers/api/login-routes.js
+++ controllers/api/login-routes.js
@@ -2,13 +2,8 @@ const router = require("express").Router();
 //const { User } = require("../models");
 
 //HOME PAGE RENDER NAME = HANDLEBARS FILE NAME
-//http://localhost:3001/
 router.get("/", (req, res) => {
     res.render("login");
-});
-//http://localhost:3001/movies = "movies" FILE NAME
-router.get("/movies", (req, res) => {
-    res.render("movies");
 });
 
 module.exports = router;
