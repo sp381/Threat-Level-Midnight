5f5a8a3e4fd681136dca2784b1884de9c0e17b6f
Carl Bush <CarlBushs13@gmail.com>
2022-05-04 17:38:56 -0700

    login.js created, login.hbs file updated with data

--- controllers/api/login-routes.js
+++ controllers/api/login-routes.js
@@ -1,3 +1,5 @@
+const { User } = require("../../models");
+
 const router = require("express").Router();
 //const { User } = require("../models");
 
@@ -5,5 +7,19 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
     res.render("login");
 });
+
+router.post("/", (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 
 module.exports = router;
