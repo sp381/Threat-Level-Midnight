150f8046da51f8aaf07625a63ca488d1cc59c87b
Carl Bush <CarlBushs13@gmail.com>
2022-05-05 11:14:37 -0700

    merge all users code

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -25,3 +25,35 @@ async function signupFormHandler(event) {
   }
 
   document.querySelector("#signup-form").addEventListener("submit", signupFormHandler);
+
+
+//SARAH'S CODE
+//   async function login(event) {
+//     event.preventDefault() 
+//     console.log('hello world');
+//     const email = document.querySelector('#exampleInputEmail1').value 
+//     const password = document.querySelector('#exampleInputPassword1').value 
+//     console.log(email);
+//     console.log(password);
+
+//     fetch('/api/user/login', {
+//         method: 'post',
+//         body: JSON.stringify({
+//             email,
+//             password, 
+//         }), 
+//         headers: {
+//             "Content-Type": "application/json", 
+//         }
+//     }).then(function() {
+//         document.location.replace('/dashboard')
+//     }).catch(function(error) {
+//         console.log('Error inside login.js catch');
+//         console.log(error);
+//     })
+// }
+
+
+// //document query selector of login form 
+
+// document.querySelector('#login-form').addEventListener('submit', login)
